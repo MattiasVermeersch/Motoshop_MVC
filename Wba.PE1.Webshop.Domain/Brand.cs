ef49920a1990932765f85676febf65ff56f58fe0
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2020-10-31 14:34:44 +0100

    Add Brand class with repo to the project

---
+++ Wba.PE1.Webshop.Domain/Brand.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PE1.Webshop.Domain
+{
+    public class Brand
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Adress { get; set; }
+        public string City { get; set; }
+        public string Zipcode { get; set; }
+        public string  Country { get; set; }
+        public string Phone { get; set; }
+    }
+}
---
+++ Wba.PE1.Webshop.Domain/BrandRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PE1.Webshop.Domain
+{
+    public class BrandRepository
+    {
+        public IEnumerable<Brand> Brands { get; set; }
+        public MotorRepository Motors { get; set; }
+
+        public BrandRepository()
+        {
+            Brands = new List<Brand>
+            {
+
+            };
+        }
+    }
+}
