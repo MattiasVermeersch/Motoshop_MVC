bdb6742c81dc99c016161ffbf32f4ec7b01aade4
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2020-10-31 16:21:48 +0100

    Add changes to Brand class and repository

--- Wba.PE1.Webshop.Domain/Brand.cs
+++ Wba.PE1.Webshop.Domain/Brand.cs
@@ -8,11 +8,7 @@ namespace PE1.Webshop.Domain
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Email { get; set; }
-        public string Adress { get; set; }
-        public string City { get; set; }
-        public string Zipcode { get; set; }
-        public string  Country { get; set; }
-        public string Phone { get; set; }
+        public string Contactpage { get; set; }
+        public string Homepage { get; set; }
     }
 }
--- Wba.PE1.Webshop.Domain/BrandRepository.cs
+++ Wba.PE1.Webshop.Domain/BrandRepository.cs
@@ -13,7 +13,41 @@ namespace PE1.Webshop.Domain
         {
             Brands = new List<Brand>
             {
-
+                new Brand
+                {
+                    Id=1, 
+                    Name="Honda Motor",
+                    Contactpage = "https://www.fl.honda.be/motorcycles/useful-links/contact-us.html",
+                    Homepage = "https://www.fl.honda.be/motorcycles.html"
+                },
+                new Brand
+                {
+                    Id=2,
+                    Name="Suzuki",
+                    Contactpage="https://www.suzuki2wheels.be/nl/content/Contact.html",
+                    Homepage="https://www.suzuki2wheels.be/nl.html?gclid=Cj0KCQjwlvT8BRDeARIsAACRFiWEKHwZL9qO0U909iTRUPgXEsR969GnCYdlOfPaeLRwKC-mKLhtSVwaAvXTEALw_wcB"
+                },
+                new Brand
+                {
+                    Id=3,
+                    Name="Kawasaki",
+                    Contactpage="https://www.kawasaki.be/nl/products/pages/Contact?Uid=0B12UAwKXlwKClENXlhQXA1aUAleCQsLUQkMX1hfCgldUAlGBgRFKi0",
+                    Homepage="https://www.kawasaki.be/nl/products"
+                },
+                new Brand
+                {
+                    Id=4,
+                    Name="Harley-Davidson",
+                    Contactpage="https://www.harley-davidson.com/be/nl/customer-service/contact-us.html",
+                    Homepage="https://www.harley-davidson.com/be/nl/index.html"
+                },
+                new Brand
+                {
+                    Id=5,
+                    Name="BMW Motorrad",
+                    Contactpage="https://www.bmw-motorrad.be/nl/public-pool/content-pool/contact.html",
+                    Homepage="https://www.bmw-motorrad.be/nl/models/modeloverview.html"
+                }
             };
         }
     }
